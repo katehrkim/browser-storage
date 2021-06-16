ebf3ecaf1a41fc0aaefbe3ffa629409936fb4088
Kate Kim <katehrkim@gmail.com>
2021-06-16 16:58:17 -0400

    Kate's answer

---
+++ script.js
@@ -0,0 +1,25 @@
+const nameKey = "name"
+
+const showGreeting = () => {
+  let nameInput = document.getElementById("input-name")
+
+  if (nameInput && nameInput.value !== "") {
+    let name = nameInput.value
+    localStorage.setItem(nameKey, name)
+    greetUser(name)
+  }
+}
+
+const greetUser = (name) => {
+  let greetingOutput = document.getElementById("output")
+  if (greetingOutput && name !== "") {
+    greetingOutput.innerHTML = "Hello " + name + "!"
+  }
+}
+
+window.onload = () => {
+  let name = localStorage.getItem(nameKey)
+  if (name && name != "") {
+    greetUser(name)
+  }
+}
